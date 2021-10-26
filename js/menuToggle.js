3fc010db0e7d555f5f440c5dac32b6dc0a05be20
rboswellj <rboswellj@gmail.com>
2021-10-26 18:30:27 -0400

    Cleaned up CSS and added smooth transition for hamburger expansion

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,12 +1,11 @@
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("open");
-    document.getElementById("header").classList.toggle("open");
 }
+
 let hamburger = document.getElementById('hamburgerBtn');
 let logo = document.getElementById('logoSm');
 let logoText = document.getElementById('logoHeading');
-hamburger.onclick = toggleMenu;
+hamburger.addEventListener("click", toggleMenu);
 logo.onclick = toggleMenu; 
 logoText.onclick = toggleMenu;
-
 
