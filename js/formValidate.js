633e142960858d65ae8bda52e411f2af01b7cdfe
rboswellj <rboswellj@gmail.com>
2021-11-03 07:25:19 -0400

    Added some form validation via css and jS. Still working on submit action.

--- js/formValidate.js
+++ js/formValidate.js
@@ -1,5 +1,36 @@
-var name = document.getElementById("nameInput");
-var email = document.getElementById("emailInput");
-var subject = document.getElementById("subjectInput");
-var message = document.getElementById("messageInput");
+function validateForm() {
+    let status = true;
+    const CFORM = document.forms.contactForm;
+    let values = {
+    name: CFORM.name.value,
+    email: CFORM.email.value,
+    subject: CFORM.subject.value,
+    message: CFORM.contactMsg.value
+    }
 
+    if (values.name===null || values.name==="") {
+        status = false;
+        document.getElementById("nameSpan").className = "error";
+    } else {
+        document.getElementById("nameSpan").className = "";
+    }
+    if (values.email===null || values.email==="") {
+        status = false;
+        document.getElementById("emailSpan").className = "error";
+    } else {
+        if (! x.includes("@")) {
+            status = false;
+            document.getElementById("emailSpan").className = "error";
+        } else {
+            document.getElementById("emailSpan").className = "";
+        }
+    }
+    if (values.message===null || values.message==="") {
+        status = false;
+        document.getElementById("messageSpan").className = "error";
+    } else {
+        document.getElementById("messageSpan").className = "";
+    }
+
+    return status
+} //end of validateForm()
