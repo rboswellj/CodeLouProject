b7519c88b98b60a7543955f33b1c3346c1d11530
Robert Johnson <RJohnson123@humana.com>
2021-11-16 18:45:06 -0500

    Everything is now working, aside from the actual email submission

--- js/formValidate.js
+++ js/formValidate.js
@@ -6,7 +6,7 @@ function validateForm() {
     email: CFORM.email.value,
     subject: CFORM.subject.value,
     message: CFORM.contactMsg.value,
-    status: true
+    status: true,
   };
 
   /* Validates entries in form */
@@ -41,37 +41,67 @@ function validateForm() {
     sessionStorage.setItem(key, value);
   }
 
+  // Reads input change and reruns validation
+  document.getElementById("nameInput").addEventListener("input", validateForm);
+  document.getElementById("emailInput").addEventListener("input", validateForm);
+  document.getElementById("messageInput").addEventListener("input", validateForm);
 
-  // console.log(sessionStorage);
-
-  // if(values.status === true) {
-  //     document.getElementById("submit").addEventListener("click", confirmForm(vals));
-  // }
 
   return values.status;
 } //end of validateForm()
 
-function confirmForm(){
-  const para = document.createElement('p');
+function confirmForm() {
+  const para = document.createElement("p");
 
   let vals = {
     name: sessionStorage.getItem("name"),
     email: sessionStorage.getItem("email"),
     subject: sessionStorage.getItem("subject"),
-    message: sessionStorage.getItem("message")
-  }
+    message: sessionStorage.getItem("message"),
+  };
 
   let fields = {
     name: document.getElementById("nameDiv"),
     email: document.getElementById("emailDiv"),
     subject: document.getElementById("subjectDiv"),
-    message: document.getElementById("messageDiv")
-  }
+    message: document.getElementById("messageDiv"),
+  };
 
   /* Populates the items on the confirm page */
   for (const [key, value] of Object.entries(vals)) {
-    fields[key].innerHTML = `<h5>${key}: </h5>` +vals[key];
+    fields[key].innerHTML = `<h5>${key}: </h5>` + vals[key];
   }
 
+  document.getElementById("editBtn").addEventListener("click", function () {
+    window.open("/contact.html", "_self");
+    editting = true;
+  });
+
+  document.getElementById('confirmBtn').addEventListener('click', function () {
+    //send via php/ajax
+    sessionStorage.clear();
+    window.open("/thanks.html", "_self");
+  });
+
 }
+
+function loadEdit() {
+  let inputCache = {
+    name: sessionStorage.getItem("name"),
+    email: sessionStorage.getItem("email"),
+    subject: sessionStorage.getItem("subject"),
+    message: sessionStorage.getItem("message"),
+  };
+
+  let formId = document.forms.contactForm;
+  formId.name.value = inputCache.name;
+  formId.email.value = inputCache.email;
+  formId.subject.value = inputCache.subject;
+  formId.contactMsg.value = inputCache.message;
+
+}
+
+
+
+
 
